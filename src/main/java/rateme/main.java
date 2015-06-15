5c2c02ed118f2e2e0f952ec725839fbd30b57146
Moritz Ellmers <moritz.ellmers@web.de>
2015-06-15 10:49:49 +0200

    changed

--- src/main/java/rateme/main.java
+++ src/main/java/rateme/main.java
@@ -6,6 +6,7 @@ package rateme;
 
 public class main {
     public static void main (String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Moritz");
+
     }
 }
