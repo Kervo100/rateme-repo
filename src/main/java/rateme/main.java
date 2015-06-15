bc77008be37de53aaee6eb60173fa746d75d5bc1
Kervo100 <kervin100@gmail.com>
2015-06-15 10:54:57 +0200

    added gadl files to Kervin

---
+++ src/main/java/rateme/main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Mo on 15.06.2015.
+ */
+
+package rateme;
+
+public class main {
+    public static void main (String[] args) {
+        System.out.println("Hello Moritz");
+
+    }
+}
