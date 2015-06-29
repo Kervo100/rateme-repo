14620943326220342a2a7aa8f84cf26cbbbc306a
Moritz Ellmers <moritz.ellmers@web.de>
2015-06-29 11:58:20 +0200

    comment added

--- src/main/java/rateme/main.java
+++ src/main/java/rateme/main.java
@@ -6,7 +6,10 @@ package rateme;
 
 public class main {
     public static void main (String[] args) {
-        System.out.println("Hello Moritz");
+        System.out.println("rateme start");
+
+
+        System.out.println("rateme close");
     }
 
 
