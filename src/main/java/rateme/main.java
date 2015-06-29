b33eefc9d992c3cff0ff67d1c3afe6d0a19a90a7
Thorben <w.thorben94@gmail.com>
2015-06-29 14:35:01 +0200

    staged for later

--- src/main/java/rateme/main.java
+++ src/main/java/rateme/main.java
@@ -5,10 +5,22 @@
 package rateme;
 
 import rateme.entity.*;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class main {
     public static void main (String[] args) {
         System.out.println("rateme start");
+
+        EntityManagerFactory emf = Persistence
+                .createEntityManagerFactory("PersistenceUnit");
+
+        EntityManager em = emf.createEntityManager();
+
+        User member = em.find(User.class, 1);
+
+        System.out.println("user: " + member.getUsername());
 
         System.out.println("rateme close");
     }
